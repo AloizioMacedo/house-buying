92cae40b3d13a8555bd0d947007ee1f827e40b1e
AloizioMacedo <atsam2@hotmail.com>
2025-06-16 17:31:05 -0300

    Implement some relevant calculations

---
+++ house_buying/calculations.py
@@ -0,0 +1,111 @@
+def calculate_money_timeseries_after_months(
+    months_to_forecast: int,
+    starting_money: float,
+    down_payment: float,
+    house_price: float,
+    house_monthly_interest: float,
+    n_months_to_pay: int,
+    money_saved_monthly: float,
+    investment_monthly_interest: float,
+):
+    """Gets the monthly timeseries of money on account after buying house.
+
+    Args:
+        months_to_forecast: How far in the future you want to see data on.
+            This can be greater or lower than the number of months
+            the payment will take.
+        starting_money: Money that is currently on the account before
+            buying the house.
+        down_payment: Starting money given at the beginning of the payment.
+        house_price: Price of the house. Should include the down_payment.
+        house_monthly_interest: Monthly interest on which the financing
+            is based upon.
+        n_months_to_pay: How many months will the payment take.
+        money_saved_monthly: How much money you are able to save on a monthly
+            basis. Should include salary and other sources of income, but
+            not the profits of investments.
+        investment_monthly_interest: Interest of the investments you put
+            your money on.
+
+    Returns:
+        A timeseries with the information, on a monthly basis, of the money
+        that will be on your account.
+    """
+    ts: list[float] = []
+
+    money_left = starting_money - down_payment
+    ts.append(money_left)
+
+    monthly_payment = calculate_monthly_payment(
+        house_price - down_payment, house_monthly_interest, n_months_to_pay
+    )
+
+    for i in range(months_to_forecast):
+        if i <= n_months_to_pay:
+            money_left -= monthly_payment
+
+        money_left = (
+            money_left * (1 + investment_monthly_interest) + money_saved_monthly
+        )
+        ts.append(money_left)
+
+    return ts
+
+
+def calculate_monthly_payment(
+    value: float,
+    monthly_interest: float,
+    n_months: int,
+    err: float = 0.001,
+    max_iters: int = 10_000,
+    upper_bound: int = 100_000,
+) -> float:
+    """Calculates the monthly payment of a given value with monthly interest.
+
+
+    Uses binary search to find the value. The last arguments are related to
+    the abstract binary search itself, not the specifics of a loan/mortgage/etc.
+
+    Args:
+        value: Total value that is being payed.
+        monthly_interest: Monthly interest rate.
+        n_months: Number of months over which the payment will occur.
+        err: Error value for when to decide to finish the binary search.
+        max_iters: Maximum iterations for the binary search before giving up.
+        upper_bound: Upper bound on the value of the monthly payment.
+
+    Returns:
+        Monthly payment that will be required to finish the payment of the
+        total value in the allocated number of months.
+    """
+    a = 0
+    b = upper_bound
+    c = (a + b) / 2
+
+    for _ in range(max_iters):
+        current_error = calculate_left(c, value, monthly_interest, n_months)
+        if abs(current_error) < err:
+            return c
+
+        if current_error > 0:
+            a = c
+        else:
+            b = c
+
+        c = (a + b) / 2
+
+    return c
+
+
+def calculate_left(
+    monthly_payment: float, total: float, monthly_interest: float, n_months: int
+):
+    """Calculates how much money is left with a given monthly payment."""
+    left = total * (1 + monthly_interest)
+    for _ in range(n_months):
+        if left <= 0:
+            left -= monthly_payment
+        else:
+            left = (left - monthly_payment) * (1 + monthly_interest)
+
+    return left
